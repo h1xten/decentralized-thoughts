2cadf0efae9c94e89c2f952884a4efe450c50735
h1xten <h1xtenc@gmail.com>
2022-10-31 22:38:18 +0300

    add base UI components

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {useNavigate} from 'react-router-dom'
-import { Box, IconButton, Typography, Button} from "@mui/material";
+import { Box, Typography, Button} from "@mui/material";
 import { AddCircleOutline, Search } from '@mui/icons-material';
 
 const Topbar = () => {
--- src/pages/homepage/Homepage.jsx
+++ src/pages/homepage/Homepage.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Typography, SvgIcon} from "@mui/material";
+import { Box, Typography} from "@mui/material";
 
 const Homepage = () => {
   return (
